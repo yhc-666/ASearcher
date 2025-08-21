083ce480cdeebe97df25b7dafdfbbd3371c843a5
“yang” <hcy@mail.com>
2025-08-21 23:29:49 +0800

    fixed utils.fs

--- AReaL/areal/utils/fs.py
+++ AReaL/areal/utils/fs.py
@@ -4,6 +4,7 @@ import os
 
 def get_user_tmp():
     user = getpass.getuser()
-    user_tmp = os.path.join("/storage/openpsi/users", user, ".cache", "realhf")
+    # Use NFS path instead of hardcoded /storage path
+    user_tmp = os.path.join("/mnt/dolphinfs/hdd_pool/docker/user/hadoop-mtsearch-assistant/ai-search/yanghaocheng04/ASearcher", ".cache", "realhf", user)
     os.makedirs(user_tmp, exist_ok=True)
     return user_tmp
